a4aac470d550ba621de505519f13497b73afaafa
Mehal-Skanda-R <mehal.skanda.rudresh@gmail.com>
2026-06-28 19:44:19 +0530

    day four learned data types and loops

---
+++ basic-1/day4-learning.py
@@ -0,0 +1,9 @@
+#match is a switch
+name = str(input("whats your name : "))
+match name:
+    case "harry":
+        print("griffindor")
+    case "hermione":
+        print("griffindor")
+    case "draco":
+        print("slytherin")
